27f20378353b403973447e1cdf307da90597bbd4
Piotr <konto.ptr@gmail.com>
2022-02-10 17:42:22 +0100

    simplified pickup

--- src/main/java/backend/ElevatorSystem.java
+++ src/main/java/backend/ElevatorSystem.java
@@ -21,65 +21,46 @@ public class ElevatorSystem {
     }
 
     public void pickup(int userPosition, DirectionsOfElevator directionOfUser) {
-        Elevator bestSameDirection = null;
-        Elevator bestRevertDirection = null;
+        Elevator bestElevator = null;
+        int distanceBetweenUserAndElevator = Integer.MAX_VALUE;
+//        chcemy minimalizować odległość lokalizacji windy:
+//        - jeśli winda jedzie w tą stronę co my chcemy to minimalizujemy odległość obecnej lokalizacji windy
+//        - jesli jedzie w przeciwną stronę to minimalizujemy odległość finalDestination, która zawsze jest najniższym/najwyższym piętrem do którego jedzie winda
+//        - jeśli winda stoji w miejscu to w zasadzie obojętne bo wtedy obecna lokalizacja i finalna są sobie równe
+
         if (directionOfUser == DirectionsOfElevator.DOWN) {
-            int distanceOfElevatorsGoingDownOrNone = Integer.MAX_VALUE;
-            // Ustawiam odległość na max, bo chcę ją minimalizować jeśli winda jedzie w tą samą stronę lub jest wolna
-            int distanceOfElevatorsGoingUp = -1;
-            // Ustawiam odległość na -1 bo chcemy maksymalizować odległość od windy która jedzie w przeciwną stronę.
-            // W przypadku gdy wiele wind jedzie do góry lepsza jest ta, która będzie dalej bo szybciej będzie zmuszona do zawrócenia,
-            // bo chcę ją minimalizować jeśli winda jedzie w tą samą stronę lub jest wolna.
 
             for (Elevator elevator : elevators) {
                 if (elevator.getCurrLevel() >= elevator.getFinalDestination()) {
-                    if (distanceOfElevatorsGoingDownOrNone > elevator.getCurrLevel() - userPosition) {
-                        distanceOfElevatorsGoingDownOrNone = elevator.getCurrLevel() - userPosition;
-                        bestSameDirection = elevator;
+                    if (distanceBetweenUserAndElevator > elevator.getCurrLevel() - userPosition) {
+                        distanceBetweenUserAndElevator = elevator.getCurrLevel() - userPosition;
+                        bestElevator = elevator;
                     }
                 } else {
-                    if (distanceOfElevatorsGoingUp < elevator.getCurrLevel() - userPosition) {
-                        distanceOfElevatorsGoingUp = elevator.getCurrLevel() - userPosition;
-                        bestRevertDirection = elevator;
+                    if (distanceBetweenUserAndElevator > elevator.getFinalDestination() - userPosition) {
+                        distanceBetweenUserAndElevator = elevator.getFinalDestination() - userPosition;
+                        bestElevator = elevator;
                     }
                 }
             }
-
-            if (distanceOfElevatorsGoingDownOrNone < distanceOfElevatorsGoingUp) {
-                bestSameDirection.addDestination(userPosition);
-            } else {
-                assert bestRevertDirection != null;
-                bestRevertDirection.addDestination(userPosition);
-            }
-
         } else {
-            int distanceOfElevatorsGoingUpOrNone = Integer.MAX_VALUE;
-            // Ustawiam odległość na max, bo chcę ją minimalizować jeśli winda jedzie w tą samą stronę lub jest wolna
-            int distanceOfElevatorsGoingDown = -1;
-            // Ustawiam odległość na -1 bo chcemy maksymalizować odległość od windy która jedzie w przeciwną stronę.
-            // W przypadku gdy wiele wind jedzie do góry lepsza jest ta, która będzie dalej bo szybciej będzie zmuszona do zawrócenia,
-            // bo chcę ją minimalizować jeśli winda jedzie w tą samą stronę lub jest wolna.
-
             for (Elevator elevator : elevators) {
                 if (elevator.getCurrLevel() <= elevator.getFinalDestination()) {
-                    if (distanceOfElevatorsGoingUpOrNone > elevator.getCurrLevel() - userPosition) {
-                        distanceOfElevatorsGoingUpOrNone = elevator.getCurrLevel() - userPosition;
-                        bestSameDirection = elevator;
+                    if (distanceBetweenUserAndElevator > elevator.getCurrLevel() - userPosition) {
+                        distanceBetweenUserAndElevator = elevator.getCurrLevel() - userPosition;
+                        bestElevator = elevator;
                     }
                 } else {
-                    if (distanceOfElevatorsGoingDown < elevator.getCurrLevel() - userPosition) {
-                        distanceOfElevatorsGoingDown = elevator.getCurrLevel() - userPosition;
-                        bestRevertDirection = elevator;
+                    if (distanceBetweenUserAndElevator > elevator.getFinalDestination() - userPosition) {
+                        distanceBetweenUserAndElevator = elevator.getFinalDestination() - userPosition;
+                        bestElevator = elevator;
                     }
                 }
             }
-            if (distanceOfElevatorsGoingUpOrNone < distanceOfElevatorsGoingDown) {
-                bestSameDirection.addDestination(userPosition);
-            } else {
-                assert bestRevertDirection != null;
-                bestRevertDirection.addDestination(userPosition);
-            }
+
         }
+        assert bestElevator != null;
+        bestElevator.addDestination(userPosition);
     }
 
 
@@ -89,9 +70,9 @@ public class ElevatorSystem {
         }
     }
 
-    public void printStep(){
-        for(ArrayList<Integer> elevatorStatus: this.status()){
-            System.out.println("Elevator nr "+ elevatorStatus.get(0) + " is at lvl "+elevatorStatus.get(1) + " and its final destination is "+elevatorStatus.get(2));
+    public void printStep() {
+        for (ArrayList<Integer> elevatorStatus : this.status()) {
+            System.out.println("Elevator nr " + elevatorStatus.get(0) + " is at lvl " + elevatorStatus.get(1) + " and its final destination is " + elevatorStatus.get(2));
         }
     }
 }
